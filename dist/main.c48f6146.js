df193c5327494423c48092c45d6049c14b218c75
doyTech <aldoyh@yahoo.com>
2022-07-29 20:28:16 +0000

    Update 7 files

--- dist/main.c48f6146.js
+++ dist/main.c48f6146.js
@@ -7751,7 +7751,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37255" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45279" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
